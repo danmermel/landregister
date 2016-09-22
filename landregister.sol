c7dd01f9f325138968370e57666aef59fd24a4d7
danielmermelstein <daniel@windermere.home>
2016-09-22 18:25:25 +0100

    got singleTransfer working

--- landregister.sol
+++ landregister.sol
@@ -1,3 +1,25 @@
+contract deed {
+ 
+  address public previousDeed;
+  address public nextDeed;
+  address public owner;
+  address public registry;
+  bool public live;
+  
+  function deed (address _previousDeed, address _owner) {
+    previousDeed = _previousDeed;
+    owner = _owner;
+    registry = msg.sender;
+    live = true;
+  }
+
+  function transferSingle(address _newDeed) {
+    if (live == false) throw;
+    if (msg.sender != registry) throw;
+    nextDeed = _newDeed;
+    live = false;
+  } 
+}
 
 
 contract landregister {
@@ -13,8 +35,10 @@ contract landregister {
   }
 
   function transferSingle(address _deedid, address _newowner) {
+    deed existing_deed = deed(_deedid);
+    if (existing_deed.owner() != msg.sender) throw;
     var newdeed = new deed(_deedid, _newowner);
     theRegister[deedCount++] = newdeed;
-    //deed.transferSingle(newdeed);  
+    existing_deed.transferSingle(newdeed);  
   }
 }
