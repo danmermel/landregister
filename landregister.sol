f9fa3a6827affb3a3670d022bd9dc31a4c943a6d
danielmermelstein <daniel@windermere.home>
2016-09-30 18:53:51 +0100

    did staging and configuration of deed

--- landregister.sol
+++ landregister.sol
@@ -4,37 +4,52 @@ contract deed {
   mapping (uint => address) public nextDeeds;
   address public owner;
   address public registry;
-  bool public live;
+  deed_status public status;
   uint public numNextDeeds;
+  string public url_to_claim;
+  string public claim_hash;
+  enum  deed_status { provisional, live, dead }
   
   function deed (address _previousDeed, address _owner) {
     previousDeed = _previousDeed;
     owner = _owner;
     registry = msg.sender;
-    live = true;
+    status = deed_status.provisional;
     numNextDeeds = 0;
+    url_to_claim ='';
+    claim_hash='';
+  }
+
+  function configure_deed (string _url, string _hash) {
+    if (status != deed_status.provisional) throw;
+    url_to_claim = _url;
+    claim_hash = _hash; 
+  } 
+
+  function commit (){
+    if (status != deed_status.provisional) throw;
+    status = deed_status.live;
   }
 
   function transferSingle(address _newDeed) {
-    if (live == false) throw;
+    if (status != deed_status.live) throw;
     if (msg.sender != registry) throw;
     nextDeeds[0] = _newDeed;
-    live = false;
     numNextDeeds = 1;
   } 
 
   function addChild(address _child) {
-    if (live == false) throw;
+    if (status != deed_status.live) throw;
     if (msg.sender != registry) throw;
     nextDeeds[numNextDeeds] = _child;
     numNextDeeds++;
   }
  
   function expire() {
-    if (live == false) throw;
+    if (status != deed_status.live) throw;
     if (msg.sender != registry) throw;
     if (numNextDeeds == 0) throw;
-    live = false;
+    status = deed_status.dead;
   }
 }
 
@@ -47,16 +62,24 @@ contract landregister {
     deedCount=0;
   }
 
-  function createDeed() {
-    theRegister[deedCount++] = new deed(0x0, msg.sender);
+  function createDeed(string  _url, string  _hash) {
+    var d = new deed(0x0, msg.sender);
+    theRegister[deedCount++] = d;
+    deed newdeed = deed(d);
+    newdeed.configure_deed (_url, _hash);
+    newdeed.commit();
   }
 
   function transferSingle(address _existing_deedid, address _newowner) {
     deed existing_deed = deed(_existing_deedid);
     if (existing_deed.owner() != msg.sender) throw;
-    var newdeed = new deed(_existing_deedid, _newowner);
-    theRegister[deedCount++] = newdeed;
-    existing_deed.transferSingle(newdeed);  
+    var newdeed_address = new deed(_existing_deedid, _newowner);
+    theRegister[deedCount++] = newdeed_address;
+    deed newdeed = deed(newdeed_address);
+    newdeed.configure_deed('scoot.co.uk', 'abc12345fgfr4567');
+    newdeed.commit();
+    existing_deed.transferSingle(newdeed_address);  
+    existing_deed.expire();
   }
 
   function split(address _existing_deedid, uint _parts) {
